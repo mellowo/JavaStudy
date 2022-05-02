fd2739f22368e6987a6e61a33926fd837982f0c3
mellowo <mellowo@icloud.com>
2022-05-02 22:48:30 +0900

    git contribution test
    Signed-off-by: mellowo <mellowo@icloud.com>

--- src/chap13/lambda/TestLambda.java
+++ src/chap13/lambda/TestLambda.java
@@ -12,7 +12,7 @@ public class TestLambda {
 
         PrintString reStr = returnString();
         reStr.showString("hello");
-        System.out.println("git test11111");
+        System.out.println("git test111111");
     }
 
     private static PrintString returnString() {
