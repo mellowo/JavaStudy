d4a559135da543a623c5fce237e82b26cb53ffaa
mellowo <mellowo@icloud.com>
2022-05-14 15:20:12 +0900

    java IO Study, Shell script exec test
    Signed-off-by: mellowo <mellowo@icloud.com>

---
+++ src/chap15/stream/test/BufferedTest2.java
@@ -0,0 +1,21 @@
+package chap15.stream.test;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class BufferedTest2 {
+    public static void main(String[] args) {
+
+        try (BufferedReader br = new BufferedReader(new FileReader("reader.txt"))) {
+
+            String line ;
+            while((line = br.readLine()) != null){
+                System.out.println(line);
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+    }
+}
